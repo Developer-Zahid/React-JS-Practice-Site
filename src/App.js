da8fa44d73bb72911762f9b2de203e9c49f6cea2
Developer-Zahid <coderexpertzahid@gmail.com>
2022-10-03 01:55:15 +0600

    Mapping data from data.json file with nested mapping

--- src/App.js
+++ src/App.js
@@ -4,11 +4,21 @@ import Card from './components/Card';
 import ArticlesData from './all-data/articles-data/data.json'
 
 function App(){
+    // ArticlesData.map((eachData, eachIndex)=>(
+    //     console.log(eachData.article_tags)
+    //     // .map((eachTag, eachTagIndex)=>(
+    //     //     console.log(eachTag.article_tags__name)
+    //     // ))
+    // ))
     return(
         <main>
             <Header />
             <section className="all-article section-gap">
-                {ArticlesData.map((eachData, eachIndex)=> <Card key={eachIndex} cardLink="./article" cardImage={eachData.article_image} cardTitle={eachData.article_title + " " + (eachIndex + 1)} cardText={eachData.article_description}/>)}
+                {
+                    ArticlesData.map((eachData, eachIndex)=> (
+                        <Card key={eachIndex} cardLink="./article" cardMetas={eachData.article_metas} cardImage={eachData.article_image} cardTitle={eachData.article_title + " " + (eachIndex + 1)} cardText={eachData.article_description}/>
+                    ))
+                }
             </section>
         </main>
     )
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,5 +1,4 @@
-function Card({cardLink, cardImage, cardTitle, cardText}){
-
+export default function Card({cardLink, cardMetas, cardImage, cardTitle, cardText}){
     let shortCardText;
 
     if(cardText.length > 90){
@@ -10,10 +9,25 @@ function Card({cardLink, cardImage, cardTitle, cardText}){
 
     return(
         <article className="card">
-            <a href={cardLink} className="card__header">
-                <img src={cardImage} alt="article image" className="card__header__image" />
-            </a>
+            <div className="card__header">
+                <a href={cardLink} className="card__header__link">
+                    <img src={cardImage} alt="article image" className="card__header__link__image" />
+                </a>
+            </div>
             <div className="card__body">
+                {
+                    cardMetas.map((eachMeta, eachMetaIndex)=>(
+                        <ul className="card__body__list" key={eachMetaIndex}>
+                            {
+                                eachMeta.article_tags__name.map((eachTag, eachTagIndex)=>(
+                                    <li key={eachTagIndex} className="card__body__list__item">
+                                        <a href="#!" className="card__body__list__item__link">{eachTag + " " + (eachTagIndex + 1)}</a>
+                                    </li>
+                                ))
+                            }
+                        </ul>
+                    ))
+                }
                 <h3 className="card__body__title">
                     <a href={cardLink} className="card__body__title__link">{cardTitle}</a>
                 </h3>
@@ -23,5 +37,3 @@ function Card({cardLink, cardImage, cardTitle, cardText}){
         </article>
     );
 }
-
-export default Card;
